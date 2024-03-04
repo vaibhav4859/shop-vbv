489b865cb1eea550ec073e15de0783accb39627f
Vaibhav <vsachdeva4859@gmail.com>
2024-03-04 22:42:02 +0530

    stripe payment added

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,8 +1,9 @@
 const Product = require("../models/product");
 const Order = require("../models/order");
-const path = require('path')
-const fs = require('fs')
-const PDFDocument = require('pdfkit');
+const path = require("path");
+const fs = require("fs");
+const PDFDocument = require("pdfkit");
+const stripe = require('stripe')(process.env.STRIPE_KEY);
 
 const ITEMS_PER_PAGE = 3;
 
@@ -12,26 +13,26 @@ exports.getProducts = (req, res, next) => {
 
   Product.find()
     .countDocuments()
-    .then(numProducts => {
+    .then((numProducts) => {
       totalItems = numProducts;
       return Product.find()
         .skip((page - 1) * ITEMS_PER_PAGE)
         .limit(ITEMS_PER_PAGE);
     })
-    .then(products => {
-      res.render('shop/product-list', {
+    .then((products) => {
+      res.render("shop/product-list", {
         prods: products,
-        pageTitle: 'Products',
-        path: '/products',
+        pageTitle: "Products",
+        path: "/products",
         currentPage: page,
         hasNextPage: ITEMS_PER_PAGE * page < totalItems,
         hasPreviousPage: page > 1,
         nextPage: page + 1,
         previousPage: page - 1,
-        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
-    .catch(err => {
+    .catch((err) => {
       const error = new Error(err);
       error.httpStatusCode = 500;
       return next(error);
@@ -61,26 +62,26 @@ exports.getIndex = (req, res, next) => {
 
   Product.find()
     .countDocuments()
-    .then(numProducts => {
+    .then((numProducts) => {
       totalItems = numProducts;
       return Product.find()
         .skip((page - 1) * ITEMS_PER_PAGE)
         .limit(ITEMS_PER_PAGE);
     })
-    .then(products => {
-      res.render('shop/index', {
+    .then((products) => {
+      res.render("shop/index", {
         prods: products,
-        pageTitle: 'Shop',
-        path: '/',
+        pageTitle: "Shop",
+        path: "/",
         currentPage: page,
         hasNextPage: ITEMS_PER_PAGE * page < totalItems,
         hasPreviousPage: page > 1,
         nextPage: page + 1,
         previousPage: page - 1,
-        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
-    .catch(err => {
+    .catch((err) => {
       const error = new Error(err);
       error.httpStatusCode = 500;
       return next(error);
@@ -182,7 +183,7 @@ exports.getOrders = (req, res, next) => {
 };
 
 exports.getInvoice = (req, res, next) => {
-  const {orderId} = req.params;
+  const { orderId } = req.params;
   Order.findById(orderId)
     .then((order) => {
       if (!order) {
@@ -202,39 +203,47 @@ exports.getInvoice = (req, res, next) => {
       );
       pdfDoc.pipe(fs.createWriteStream(invoicePath));
       pdfDoc.pipe(res);
-      pdfDoc.fontSize(30).text('INVOICE',20,30);
-      pdfDoc.fontSize(30).text('SHOP-VBV',20,30,{align:'right'});
-      pdfDoc.text('_______________________________');
-      pdfDoc.text(' ');
-      pdfDoc.fontSize(14).text('Invoice #'+ order.user.userId,20,110);
-      pdfDoc.fontSize(14).text('Invoice Date: '+ new Date().toLocaleString());
-      pdfDoc.fontSize(14).text('Invoice To: '+ order.user.email);
-      pdfDoc.fontSize(30).text('_______________________________',20,140);
-      pdfDoc.fontSize(14).text(' ');
-      
-      pdfDoc.fontSize(16).text('   ITEM                  DESCRIPTION                       Qty               AMOUNT',20,175);
-      
-      pdfDoc.fontSize(30).text('_______________________________',20,170);
+      pdfDoc.fontSize(30).text("INVOICE", 20, 30);
+      pdfDoc.fontSize(30).text("SHOP-VBV", 20, 30, { align: "right" });
+      pdfDoc.text("_______________________________");
+      pdfDoc.text(" ");
+      pdfDoc.fontSize(14).text("Invoice #" + order.user.userId, 20, 110);
+      pdfDoc.fontSize(14).text("Invoice Date: " + new Date().toLocaleString());
+      pdfDoc.fontSize(14).text("Invoice To: " + order.user.email);
+      pdfDoc.fontSize(30).text("_______________________________", 20, 140);
+      pdfDoc.fontSize(14).text(" ");
+
+      pdfDoc
+        .fontSize(16)
+        .text(
+          "   ITEM                  DESCRIPTION                       Qty               AMOUNT",
+          20,
+          175
+        );
+
+      pdfDoc.fontSize(30).text("_______________________________", 20, 170);
       let totalPrice = 0;
       let item = 1;
       let fsize = 14;
       let ystart = 210;
       let xstart = 40;
       let yinc = fsize + 20;
-      let ycoord = ystart + (item-1)*yinc;
-      order.products.forEach(prod => {
+      let ycoord = ystart + (item - 1) * yinc;
+      order.products.forEach((prod) => {
         totalPrice += prod.quantity * prod.product.price;
-        
-         pdfDoc.fontSize(fsize).text(' '+ item, xstart ,ycoord );
-         pdfDoc.fontSize(fsize).text(prod.product.title, xstart + 120 ,ycoord );
-         pdfDoc.fontSize(fsize).text(prod.quantity, xstart + 330 ,ycoord );
-         pdfDoc.fontSize(fsize).text(prod.product.price, xstart + 430 ,ycoord );
-         item++;
-         ycoord = ystart + (item-1)*fsize;
-      })
-      pdfDoc.fontSize(30).text('_______________________________',20,ycoord);
-      pdfDoc.fontSize(20).text(' Total: $' + totalPrice, 400,ycoord+40);
-      pdfDoc.fontSize(30).text('_______________________________', 20,ycoord+40);
+
+        pdfDoc.fontSize(fsize).text(" " + item, xstart, ycoord);
+        pdfDoc.fontSize(fsize).text(prod.product.title, xstart + 120, ycoord);
+        pdfDoc.fontSize(fsize).text(prod.quantity, xstart + 330, ycoord);
+        pdfDoc.fontSize(fsize).text(prod.product.price, xstart + 430, ycoord);
+        item++;
+        ycoord = ystart + (item - 1) * fsize;
+      });
+      pdfDoc.fontSize(30).text("_______________________________", 20, ycoord);
+      pdfDoc.fontSize(20).text(" Total: $" + totalPrice, 400, ycoord + 40);
+      pdfDoc
+        .fontSize(30)
+        .text("_______________________________", 20, ycoord + 40);
       pdfDoc.end();
       // fs.readFile(invoicePath, (err, data) => {
       //   if (err) {
@@ -252,4 +261,100 @@ exports.getInvoice = (req, res, next) => {
       // file.pipe(res);
     })
     .catch((err) => next(err));
+};
+
+exports.getCheckout = (req, res, next) => {
+  let products;
+  let total = 0;
+  req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      products = user.cart.items;
+      total = 0;
+      products.forEach((p) => {
+        total += p.quantity * p.productId.price;
+      });
+
+      //   return stripe.checkout.sessions.create({
+      //     payment_method_types: ["card"],
+      //     line_items: products.map((p) => {
+      //       return {
+      //         name: p.productId.title,
+      //         description: p.productId.description,
+      //         amount: p.productId.price * 100,
+      //         currency: "usd",
+      //         quantity: p.quantity,
+      //       };
+      //     }),
+      //     success_url:
+      //       req.protocol + "://" + req.get("host") + "/checkout/success", // => http://localhost:3000
+      //     cancel_url: req.protocol + "://" + req.get("host") + "/checkout/cancel",
+      //   });
+      // })
+      return stripe.checkout.sessions.create({
+        line_items: products.map((p) => {
+          return {
+            price_data: {
+              currency: "inr",
+              unit_amount: parseInt(Math.ceil(p.productId.price * 100)),
+              product_data: {
+                name: p.productId.title,
+                description: p.productId.description,
+              },
+            },
+            quantity: p.quantity,
+          };
+        }),
+
+        mode: "payment",
+
+        success_url:
+          req.protocol + "://" + req.get("host") + "/checkout/success", // => http://localhost:3000,
+
+        cancel_url: req.protocol + "://" + req.get("host") + "/checkout/cancel",
+      });
+    })
+    .then((session) => {
+      res.render("shop/checkout", {
+        path: "/checkout",
+        pageTitle: "Checkout",
+        products: products,
+        totalSum: total,
+        sessionId: session.id,
+      });
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
+  req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items.map((i) => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          email: req.user.email,
+          userId: req.user,
+        },
+        products: products,
+      });
+      return order.save();
+    })
+    .then((result) => {
+      return req.user.clearCart();
+    })
+    .then(() => {
+      res.redirect("/orders");
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
--- routes/shop.js
+++ routes/shop.js
@@ -11,8 +11,10 @@ router.get("/products/:productId", shopController.getProduct);
 router.get("/cart", isAuth, shopController.getCart);
 router.post("/cart", isAuth, shopController.postCart);
 router.post("/cart-delete-item", isAuth, shopController.postCartDeleteProduct);
-router.post("/create-order", isAuth, shopController.postOrder);
 router.get("/orders", isAuth, shopController.getOrders);
 router.get('/orders/:orderId', isAuth, shopController.getInvoice);
+router.get('/checkout', isAuth, shopController.getCheckout);
+router.get('/checkout/success', shopController.getCheckoutSuccess);
+router.get('/checkout/cancel', shopController.getCheckout);
 
 module.exports = router;
